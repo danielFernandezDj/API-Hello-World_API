6c002001937717402d7ac899231ea627caf0b8ce
danielFernandezJd <mdanif9728@gmail.com>
2024-05-26 15:58:57 -0700

    Initial commit of local project files

--- server.js
+++ server.js
@@ -28,28 +28,3 @@ const languagesController = require('./controllers/languages_controller.js')
 app.use('/languages', languagesController)
 
 
-
-// DEPENDENCIES
-const express = require('express')
-const mongoose = require('mongoose')
-
-// CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-)
-
-// MIDDLEWARE
-app.use(express.urlencoded({extended: true}))
-
-// ROUTES
-app.get('/', (req, res) => {
-  res.send('Welcome to the Hello World! API')
-})
-
-// LISTEN
-app.listen(PORT, () => {
-  console.log('Greetings! From port: ', PORT);
-})
